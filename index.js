ab12f2792f645cc783b5f8d36779738e0e343496
faruk <studentfaruk0308@gmail.com>
2022-03-28 22:39:18 +1100

    After completion

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+console.log(writeCards(["Ami", "Tumi", "She"], "Wedding"));
+
+  function countDown(startingNumber) {
+    while ( startingNumber > 0 ) {
+      console.log(`I am the count down number ${startingNumber}`);
+      startingNumber --;
+    }
+    console.log(`I am the last number ${startingNumber}`);
+  }
+
+countDown(9);
